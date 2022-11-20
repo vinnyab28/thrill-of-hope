f7c0e12bb49e5beb062787840d7e7eb1d5e7b308
Vineeth Prakash <gudipallivineethprakash@karunya.edu.in>
2022-11-20 21:59:39 +0530

    Added splash screen

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 $(document).ready(() => {
-	const background_music = $("#backgroundMusic");
+	setTimeout(() => {
+		$(".splash-screen").addClass("hidden");
+	}, 2000);
+
 	$(".registration-link").click(() => {
 		var pc_visitor_form = "https://wccbangalore.churchcenter.com/people/forms/467901";
 		ChurchCenterModal.open(pc_visitor_form);
