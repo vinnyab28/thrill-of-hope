a6b347326ab09dd7f582a584eb3afd70022eb381
Vineeth Prakash <gudipallivineethprakash@karunya.edu.in>
2022-11-20 21:41:58 +0530

    Added background music

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("Hello world!");
+$(document).ready(() => {
+	const background_music = $("#backgroundMusic");
+	$(".registration-link").click(() => {
+		var pc_visitor_form = "https://wccbangalore.churchcenter.com/people/forms/467901";
+		ChurchCenterModal.open(pc_visitor_form);
+	});
+
+	$(".background-music-btn").click(() => {
+		$("#backgroundMusic").prop("muted", !$("#backgroundMusic").prop("muted"));
+	});
+});
