2aaf5bb7c27a3279a22825ce9bd334055b7dcf40
Vineeth Prakash <gudipallivineethprakash@karunya.edu.in>
2022-11-20 22:44:36 +0530

    Added snowfall

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
 $(document).ready(() => {
+	//Can run the snowfall by calling:
 	setTimeout(() => {
 		$(".splash-screen").addClass("hidden");
-	}, 2000);
+		snowfall.start({
+			bg: "transparent",
+			primary: "#8d90b7",
+			secondary: "#ffffff",
+			density: 106,
+			wave: {
+				frequency: 0.1,
+				amplitude: 1.61,
+			},
+			gravity: {
+				angle: 90,
+				strength: 0.2,
+			},
+			wind: {
+				angle: 130,
+				strength: 0.5,
+			},
+		});
+	}, 1500);
 
 	$(".registration-link").click(() => {
 		var pc_visitor_form = "https://wccbangalore.churchcenter.com/people/forms/467901";
